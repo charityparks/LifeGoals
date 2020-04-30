bdc7484a70703077cec83f51a25c421c6a1d456a
Charity Parks <54159411+charityparks@users.noreply.github.com>
2020-04-30 19:11:27 -0400

    Add files via upload

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,17 @@
 class ActivitiesController < ApplicationController
 
   def index
-    render :json => Activity.all, :include => :category
+    render :json => Activity.all, :include => :category, status => 200
+  end
+
+  def create
+    category = Category.find_by(name: params[:category].downcase)
+    activity = Activity.create(name: params[:name], category: category)
+    render :json => activity, :status => 201
+  end
+
+  def destroy
+    activity = Activity.find(params[:id])
+        render :json => {id: params[:id], message: "Record was successfully removed"}, :status => 200
   end
 end
