bef458796d264340b9ffe0f527f6e19eb1c96ea4
Charity Parks <54159411+charityparks@users.noreply.github.com>
2020-05-08 15:27:06 -0400

    picture added at bottom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 const app = new StorageBox;
 AppAdapter.getActivities();
 app.bindEventListeners();
-
-
-   // let clickPlayer = () => {
-   //  return (newPlayer.value ? addPlayer(newPlayer.value) : alert("cannot be nameless"));
-   //  };
